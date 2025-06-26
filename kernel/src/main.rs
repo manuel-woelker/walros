a0d54c3fc8b9d4f648771fc59d45f4bfbb573b6f
Manuel Woelker <manuel.woelker+jj@gmail.com>
2025-06-26 23:09:23 +0200

    feat(kernel): basic println! and panic! impls writing to VGA buffer (from https://os.phil-opp.com/vga-text-mode/ )

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,27 +3,22 @@
 // disable default main entry point
 #![cfg_attr(not(test), no_main)]
 
+mod vga_buffer;
+
+#[allow(dead_code)]
 static HELLO: &[u8] = b"Hello Wild World!";
 
 #[allow(clippy::empty_loop)]
 #[cfg(not(test))]
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {}
+    panic!("Hello World{}", "!");
 }
 /// Install panic Handler, simply loop forever
 #[allow(clippy::empty_loop)]
 #[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    println!("KERNEL PANIC: {}", info);
     loop {}
 }
--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 TARGET=--target=x86_64-kernel.json
-
+export CARGO_MANIFEST_DIR=$(dirname $0)/..
 #rustup run nightly cargo build --package walros-kernel $TARGET
 rustup run nightly cargo bootimage $TARGET
